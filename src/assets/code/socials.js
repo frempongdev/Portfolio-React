f24cc33f5f2b7ae0a02c71fe1f62bdc2434a37b8
Frempong <samankwaa.frempong@gmail.com>
2024-01-31 12:37:21 +0000

    Add helper file to hold social object

---
+++ src/assets/code/socials.js
@@ -0,0 +1,44 @@
+export const socials = [
+    {
+        name: 'LinkedIn',
+        icon: 'src/assets/socials/linkedin-logo.png',
+        username: 'Samuel Amankwaa-Frempong',
+        link: 'https://www.linkedin.com/in/frempongdev/'
+    },
+    {
+        name: 'Github',
+        icon: 'src/assets/socials/github.png',
+        username: 'frempongdev',
+        link: 'https://github.com/frempongdev'
+    },
+    {
+        name: 'X',
+        icon: 'src/assets/socials/twitter.png',
+        username: 'knobaddy',
+        link: 'https://twitter.com/knobaddy'
+    },
+    {
+        name: 'Instagram',
+        icon: 'src/assets/socials/instagram.png',
+        username: 'frempongdev',
+        link: 'https://www.instagram.com/frempongdev/'
+    },
+    {
+        name: 'Facebook',
+        icon: 'src/assets/socials/facebook.png',
+        username: 'Samuel Amankwaa-Frempong',
+        link: 'https://web.facebook.com/braknower'
+    },
+    {
+        name: 'Medium',
+        icon: 'src/assets/socials/medium.png',
+        username: 'Samuel Amankwaa-Frempong',
+        link: 'https://medium.com/@samankwaa.frempong'
+    },
+    {
+        name: 'Wellfound',
+        icon: 'src/assets/socials/linkedin-logo.png',
+        username: 'frempongdev',
+        link: 'https://wellfound.com/'
+    },
+]
