38ebadd3b046a67052e10a6f8faffb3be27eb0ce
Frempong <samankwaa.frempong@gmail.com>
2024-05-13 12:02:37 +0000

    Update App.jsx with Redux store Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import Left from './components/Left'
 import Right from './components/Right'
 import './App.css'
+import { Provider } from 'react-redux'
+import store from './components/redux/store'
 
 function App() {
 
   return (
-    <div className="full-page">
-      <Left />
-      <Right />
-      
-    </div>
+    <Provider store={store}>
+      <div className='mobile-wait'>
+        <div className="mob-notice">
+        <div className="image-circle mnp">
+            <img src="https://avatars.githubusercontent.com/u/117144028?v=4" alt="me-pic" className="me-pic" />
+          </div>
+        </div>
+        <div className="mob-notice">MOBILE VERSION STILL IN PRODUCTION</div>
+      </div>
+      <div className="full-page">
+        <Left />
+        <Right />
+      </div>
+    </Provider>
   )
 }
 
