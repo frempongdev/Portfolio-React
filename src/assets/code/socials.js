1b14c6cfc7887c7ba43377ef03b73443d7a25f85
Frempong <samankwaa.frempong@gmail.com>
2024-05-13 18:44:28 +0000

    Change direct image source of social icons to file imports

--- src/assets/code/socials.js
+++ src/assets/code/socials.js
@@ -1,43 +1,51 @@
+import linkedinIcon from '../socials/linkedin-logo.png'
+import gihubIcon from '../socials/github.png'
+import xIcon from '../socials/twitter.png'
+import instaIcon from '../socials/instagram.png'
+import fbIcon from '../socials/facebook.png'
+import mediumIcon from '../socials/medium.png'
+import wellfoundIcon from '../socials/wellfound-logo.png'
+
 export const socials = [
     {
         name: 'LinkedIn',
-        icon: 'src/assets/socials/linkedin-logo.png',
+        icon: linkedinIcon,
         username: 'Samuel Amankwaa-Frempong',
         link: 'https://www.linkedin.com/in/frempongdev/'
     },
     {
         name: 'Github',
-        icon: 'src/assets/socials/github.png',
+        icon: gihubIcon,
         username: 'frempongdev',
         link: 'https://github.com/frempongdev'
     },
     {
         name: 'X',
-        icon: 'src/assets/socials/twitter.png',
+        icon: xIcon,
         username: 'knobaddy',
         link: 'https://twitter.com/knobaddy'
     },
     {
         name: 'Instagram',
-        icon: 'src/assets/socials/instagram.png',
+        icon: instaIcon,
         username: 'frempongdev',
         link: 'https://www.instagram.com/frempongdev/'
     },
     {
         name: 'Facebook',
-        icon: 'src/assets/socials/facebook.png',
+        icon: fbIcon,
         username: 'Samuel Amankwaa-Frempong',
         link: 'https://web.facebook.com/braknower'
     },
     {
         name: 'Medium',
-        icon: 'src/assets/socials/medium.png',
+        icon: mediumIcon,
         username: 'Samuel Amankwaa-Frempong',
         link: 'https://medium.com/@samankwaa.frempong'
     },
     {
         name: 'Wellfound',
-        icon: 'src/assets/socials/wellfound-logo.png',
+        icon: wellfoundIcon,
         username: 'frempongdev',
         link: 'https://wellfound.com/'
     },
